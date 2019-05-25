0f2423dfdbaed1e86dbc6591ffad07cdd925a356
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2019-05-25 11:00:59 -0500

    Guarda una imagen y permite pintar de cualquier color

---
+++ src/ui/Main.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("window.fxml"))));
+        stage.setTitle("Paint");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
