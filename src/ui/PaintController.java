a10da49d4615d914780cb478eb9ec18f0ec9846d
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2019-05-25 11:19:13 -0500

    Reemplazado ComboBox por TextField para el tamaño del lapiz

--- src/ui/PaintController.java
+++ src/ui/PaintController.java
@@ -7,9 +7,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
-
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
@@ -23,16 +22,40 @@ public class PaintController {
     private ColorPicker colorPicker;
 
     @FXML
-    private TextField brushSize;
-
-    @FXML
     private CheckBox eraser;
+    
+    @FXML
+    private ComboBox<String> brushSize;
 
     public void initialize() {
-        GraphicsContext g = canvas.getGraphicsContext2D();
+        brushSize.getItems().add("8");
+        brushSize.getItems().add("9");
+        brushSize.getItems().add("10");
+        brushSize.getItems().add("11");
+        brushSize.getItems().add("12");
+        brushSize.getItems().add("14");
+        brushSize.getItems().add("16");
+        brushSize.getItems().add("18");
+        brushSize.getItems().add("20");
+        brushSize.getItems().add("22");
+        brushSize.getItems().add("24");
+        brushSize.getItems().add("26");
+        brushSize.getItems().add("28");
+        brushSize.getItems().add("32");
+        brushSize.getItems().add("36");
+        brushSize.getItems().add("40");
+        brushSize.getItems().add("44");
+        brushSize.getItems().add("48");
+        brushSize.getItems().add("52");
+        brushSize.getItems().add("60");
+        brushSize.getItems().add("68");
+        brushSize.getItems().add("72");
+        brushSize.getItems().add("78");
+        brushSize.getItems().add("82");
+    	GraphicsContext g = canvas.getGraphicsContext2D();
 
         canvas.setOnMouseDragged(e -> {
-            double size = Double.parseDouble(brushSize.getText());
+            double size = Double.parseDouble(brushSize.getValue());
             double x = e.getX() - size / 2;
             double y = e.getY() - size / 2;
 
