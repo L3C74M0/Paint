88f73ec4f7a5c2d846a6c72cf2e100070aba271c
Jesus Villota <danielvillota1016@gmail.com>
2019-05-25 19:05:33 -0500

    Load images function and proper documentation added.

--- src/ui/PaintController.java
+++ src/ui/PaintController.java
@@ -2,6 +2,7 @@ package ui;
 
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -9,8 +10,11 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
+
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -28,6 +32,12 @@ public class PaintController {
     @FXML
     private ComboBox<String> brushSize;
 
+    
+    /**
+     * This method initializes the options to choose the pencil size and sets 
+     * the pane to enable the user to paint or erase over it.
+     */
+    @FXML
     public void initialize() {
     	brushSize.setValue("12");
         brushSize.getItems().add("8");
@@ -70,23 +80,48 @@ public class PaintController {
         });
 	}
     
+    /**
+     * This method allows the user to choose an image from file system and add it to the canvas,
+     * where it's possible to draw over it.
+     * Pre: an image with .jgp or .png format has to be selected and the <<OK>> button in the 
+     * file chooser has to be pressed, otherwise the image isn't going to be loaded.
+     * Post: the image is going to be displayed in the canvas.
+     * @param event the event received after clicking on the <<Load image>> button.
+     */
+    @FXML
+    void loadImage(ActionEvent event) {
+    	JFileChooser fc = new JFileChooser();
+    	fc.addChoosableFileFilter(new FileNameExtensionFilter(".Images","jpg", "png", "PNG", "JPEG", "JPG"));
+    	fc.setApproveButtonText("Load");
+    	int result = fc.showSaveDialog(null);
+    	if(result == JFileChooser.APPROVE_OPTION) {
+    		Image img = new Image(fc.getSelectedFile().toURI().toString(), canvas.getWidth(), canvas.getHeight(), false, true);
+        	canvas.getGraphicsContext2D().drawImage(img, 0, 0);
+    	}
+    }
     
-    /*
-     * El metodo sirve para guardar la imagen que se ha dibujado
-     * Se puede escoger la ruta donde sera guardado el archivo
+    /**
+     * This method allows to save the drawn image in the specified path.
+     * Pre: the specified path has to exist in the file system.
+     * Post: The image will be saved successfully.
      */
     public void onSave() {
         try {
             Image snapshot = canvas.snapshot(null, null);
             JFileChooser saveAs = new JFileChooser();
-            saveAs.setApproveButtonText("Guardar");
+            saveAs.setApproveButtonText("Save");
             saveAs.showSaveDialog(null);
             ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File(saveAs.getSelectedFile()+".png"));
         } catch (IOException e) {
             System.out.println("Failed to save image");
         }
     }
+    
+    
 
+    /**
+     * This methods allows to close the program when the exit button is pressed.
+     */
     public void onExit() {
         Platform.exit();
     }
