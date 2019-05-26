c1ba138930019db8f8c17a2edde2cbcdb6dae3af
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2019-05-26 16:26:52 -0500

    The image loads the size of the canvas

--- src/ui/PaintController.java
+++ src/ui/PaintController.java
@@ -17,128 +17,140 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PaintController {
+
+	@FXML
+	private BorderPane borderPane;
+
+	@FXML
+	private Canvas canvas;
+
+	@FXML
+	private ColorPicker colorPicker;
+
+	@FXML
+	private CheckBox eraser;
+
+	@FXML
+	private ComboBox<String> brushSize;
+
+	@FXML
+	private Circle circle;
 	
-    @FXML
-    private BorderPane borderPane;
-    
-    @FXML
-    private Canvas canvas;
+	private ArrayList<Circle> circles;
 
-    @FXML
-    private ColorPicker colorPicker;
+	/**
+	 * This method initializes the options to choose the pencil size and sets the
+	 * pane to enable the user to paint or erase over it.
+	 */
+	@FXML
+	public void initialize() {
+		circles = new ArrayList<Circle>();
+		brushSize.setValue("12");
+		brushSize.getItems().add("2");
+		brushSize.getItems().add("4");
+		brushSize.getItems().add("6");
+		brushSize.getItems().add("8");
+		brushSize.getItems().add("9");
+		brushSize.getItems().add("10");
+		brushSize.getItems().add("11");
+		brushSize.getItems().add("12");
+		brushSize.getItems().add("14");
+		brushSize.getItems().add("16");
+		brushSize.getItems().add("18");
+		brushSize.getItems().add("20");
+		brushSize.getItems().add("22");
+		brushSize.getItems().add("24");
+		brushSize.getItems().add("26");
+		brushSize.getItems().add("28");
+		brushSize.getItems().add("32");
+		brushSize.getItems().add("36");
+		brushSize.getItems().add("40");
+		brushSize.getItems().add("44");
+		brushSize.getItems().add("48");
+		brushSize.getItems().add("52");
+		brushSize.getItems().add("60");
+		brushSize.getItems().add("68");
+		brushSize.getItems().add("72");
+		brushSize.getItems().add("78");
+		brushSize.getItems().add("82");
+		GraphicsContext g = canvas.getGraphicsContext2D();
+		canvas.setOnMouseDragged(e -> {
+			double size = Double.parseDouble(brushSize.getValue());
+			double x = e.getX() - size / 2;
+			double y = e.getY() - size / 2;
 
-    @FXML
-    private CheckBox eraser;
-    
-    @FXML
-    private ComboBox<String> brushSize;
-    
-    @FXML
-    private Circle circles1;
-    
-    //private ArrayList<Circle> circles;
-
-    
-    /**
-     * This method initializes the options to choose the pencil size and sets 
-     * the pane to enable the user to paint or erase over it.
-     */
-    @FXML
-    public void initialize() {
-    	brushSize.setValue("12");
-    	brushSize.getItems().add("2");
-    	brushSize.getItems().add("4");
-    	brushSize.getItems().add("6");
-        brushSize.getItems().add("8");
-        brushSize.getItems().add("9");
-        brushSize.getItems().add("10");
-        brushSize.getItems().add("11");
-        brushSize.getItems().add("12");
-        brushSize.getItems().add("14");
-        brushSize.getItems().add("16");
-        brushSize.getItems().add("18");
-        brushSize.getItems().add("20");
-        brushSize.getItems().add("22");
-        brushSize.getItems().add("24");
-        brushSize.getItems().add("26");
-        brushSize.getItems().add("28");
-        brushSize.getItems().add("32");
-        brushSize.getItems().add("36");
-        brushSize.getItems().add("40");
-        brushSize.getItems().add("44");
-        brushSize.getItems().add("48");
-        brushSize.getItems().add("52");
-        brushSize.getItems().add("60");
-        brushSize.getItems().add("68");
-        brushSize.getItems().add("72");
-        brushSize.getItems().add("78");
-        brushSize.getItems().add("82");
-    	GraphicsContext g = canvas.getGraphicsContext2D();
-    	canvas.setOnMouseDragged(e -> {
-            double size = Double.parseDouble(brushSize.getValue());
-            double x = e.getX() - size / 2;
-            double y = e.getY() - size / 2;
-
-            if (eraser.isSelected()) {
-                g.clearRect(x, y, size, size);
-            } else {
-                g.setFill(colorPicker.getValue());
-                g.fillRect(x, y, size, size);
-            }
-        });
+			if (eraser.isSelected()) {
+				g.clearRect(x, y, size, size);
+			} else {
+				g.setFill(colorPicker.getValue());
+				g.fillRect(x, y, size, size);
+			}
+		});
 	}
 
-    /**
-     * This method allows the user to choose an image from file system and add it to the canvas,
-     * where it's possible to draw over it.
-     * Pre: an image with .jgp or .png format has to be selected and the <<OK>> button in the 
-     * file chooser has to be pressed, otherwise the image isn't going to be loaded.
-     * Post: the image is going to be displayed in the canvas.
-     * @param event the event received after clicking on the <<Load image>> button.
-     */
+	/**
+	 * This method allows the user to choose an image from file system and add it to
+	 * the canvas, where it's possible to draw over it. Pre: an image with .jgp or
+	 * .png format has to be selected and the <<OK>> button in the file chooser has
+	 * to be pressed, otherwise the image isn't going to be loaded. Post: the image
+	 * is going to be displayed in the canvas.
+	 * 
+	 * @param event the event received after clicking on the <<Load image>> button.
+	 */
+	@FXML
+	void loadImage(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+		FileChooser.ExtensionFilter extFilterjpg = new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
+		FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
+		FileChooser.ExtensionFilter extFilterpng = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+		fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
+		File file = fileChooser.showOpenDialog(null);
+
+		try {
+			BufferedImage bufferedImage = ImageIO.read(file);
+			Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+			canvas.getGraphicsContext2D().drawImage(image, 0, 0, 800, 624);
+		} catch (IOException e) {
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	
     @FXML
-    void loadImage(ActionEvent event) {
-    	FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilterJPG =  new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
-        FileChooser.ExtensionFilter extFilterjpg =  new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
-        FileChooser.ExtensionFilter extFilterPNG =  new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
-        FileChooser.ExtensionFilter extFilterpng = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
-        fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
+    void insertCircle(ActionEvent event) {
+    //	canvas.fillCircle(canvas.getOnMouseDragged(), currentYLocation, getDiameter());
+    }
+	
+	
+	
+	/**
+	 * This method allows to save the drawn image in the specified path. Pre: the
+	 * specified path has to exist in the file system. Post: The image will be saved
+	 * successfully.
+	 */
+	public void onSave() {
+		try {
+			Image snapshot = canvas.snapshot(null, null);
+			FileChooser saveAs = new FileChooser();
+			saveAs.setTitle("Save");
+			FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+			FileChooser.ExtensionFilter extFilterjpg = new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
+			FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
+			FileChooser.ExtensionFilter extFilterpng = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+			saveAs.getExtensionFilters().addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
+			ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File(saveAs.showSaveDialog(null) + ""));
+		} catch (IOException e) {
+		} catch (IllegalArgumentException e) {
+		}
+	}
 
-        File file = fileChooser.showOpenDialog(null);
-        
-        try {
-            BufferedImage bufferedImage = ImageIO.read(file);
-            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-            canvas.getGraphicsContext2D().drawImage(image, 0, 0);
-        } catch (IOException e) {}
-    }
-    
-    /**
-     * This method allows to save the drawn image in the specified path.
-     * Pre: the specified path has to exist in the file system.
-     * Post: The image will be saved successfully.
-     */
-    public void onSave() {
-        try {
-            Image snapshot = canvas.snapshot(null, null);
-            FileChooser saveAs = new FileChooser();
-            saveAs.setTitle("Save");
-            FileChooser.ExtensionFilter extFilterJPG =  new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
-            FileChooser.ExtensionFilter extFilterjpg =  new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
-            FileChooser.ExtensionFilter extFilterPNG =  new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
-            FileChooser.ExtensionFilter extFilterpng = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
-            saveAs.getExtensionFilters().addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
-            ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File(saveAs.showSaveDialog(null)+""));
-        } catch (IOException e) {}
-    }
-
-    /**
-     * This methods allows to close the program when the exit button is pressed.
-     */
-    public void onExit() {
-        Platform.exit();
-    }
+	/**
+	 * This methods allows to close the program when the exit button is pressed.
+	 */
+	public void onExit() {
+		Platform.exit();
+	}
 }
